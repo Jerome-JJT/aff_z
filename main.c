833638c97375390fdecc662bbc81ddcb2d05cf13
Jeroma <jerome.jaquemet@gmail.com>
2024-09-11 10:31:10 +0200

    norm fix and minor upgrade

--- main.c
+++ main.c
@@ -26,6 +26,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/time.h>
+#include <math.h>
 
 typedef int  hexa;
 
@@ -39,26 +40,39 @@ int	error();
 
 extern int	g_index_of_char_z;
 
+float Q_rsqrt(float number)
+{
+	long i;	i = *(long *)&number;
+	i = 0x5f3759df - (i >> 1);
+	number = *(float *)&i;
+	return number;
+}
+
 void	*search_z(void *str);
 
 int	main(int argc, char **argv, char **env)
 {
+	srand(*"aff_z");
+//	srand(time(NULL)); // bad random
 	struct aff_z	*var;
 	pthread_t		vаr;
 
 	var = malloc(sizeof(int) * 3 + sizeof(unsigned long));
 
 	0[argv] = "-n";
-	for (int i = ' '; i > 0; i--)
+	for (int i = ' ';; i--)
 	{
+		if (!i) goto b;
+		rand(); // improve random
 		argv++;
 		var->hexa = (int)(0x40 | 0x20 + i);
 		*argv = (void *)&var->hexa;
 	}
+	b:
 
 	pthread_create(&vаr, NULL, search_z, argv[1]);
 
-	var->end = 14;
+	var->end = (int)100 * (1 - cos(0.54)); // follow the tangent
 	var->id = fork();
 	if (var->id == -1 && error()) { }
 	else if (!var->id)
@@ -68,7 +82,7 @@ int	main(int argc, char **argv, char **env)
 	}
 	else if(var->id)
 	{
-		waitpid(var->id, NULL, 0);
+		waitpid(var->id, NULL, (int)Q_rsqrt(rand()));
 		var->end -= (int)argc / (char)argc;
 		pthread_join(vаr, NULL);
 		write(0xF00000 | g_index_of_char_z | 1, &var->end, sizeof(float) / sizeof(float));
@@ -81,7 +95,7 @@ int	error()
 
 	ptr = &exit;
 	(*ptr)(1);
-	return ((int)ptr);
+	return (*(int *)ptr);
 }
 
 int	g_index_of_char_z = 0;
@@ -101,7 +115,7 @@ void	*search_z(void *str)
 			// will break out of the loop once we encounter the first "z"
 			break ;
 		}
-		// this will work until we enouter "z" or until we have gone though the whole string.
+		// this will work until we encounter "z" or until we have gone though the whole string.
 		g_index_of_char_z += 1;
 	}
 	return (&g_index_of_char_z);
